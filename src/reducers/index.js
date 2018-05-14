6c75a9e1fc82f6bfc836f4e5747cd7bfa2ab19e9
adapass182 <adampassingham90@gmail.com>
2018-05-14 16:11:24 +0200

    removed duplicate reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,17 +1,6 @@
-<<<<<<< HEAD
 import likeDislike from './likeDislikeDog_reducer'
 import fetchedImage from './fetchedImage_reducer'
 
 export default {
   likeDislike,
   fetchedImage
-=======
-import activePhoto from './activePhoto_reducer'
-import likeDisklikePhoto from './likeDisklikePhoto_reducer'
-
-
-export default {
-  activePhoto,
-  likeDisklikePhoto
->>>>>>> 2298f55fd7347ee05aa4f7b997b7687bcd8adc6f
-}
