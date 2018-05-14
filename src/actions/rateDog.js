ba08db48b32ed1fb189c1b90846956c6920de903
zofejes <zofejes@gmail.com>
2018-05-14 15:45:22 +0200

    rateDog action creator finished

--- src/actions/rateDog.js
+++ src/actions/rateDog.js
@@ -7,42 +7,25 @@ export const LIKE_DOG = 'LIKE_DOG'
 export const DISLIKE_DOG = 'DISLIKE_DOG'
 
 export const getDog = () => (dispatch) => {
-  console.log('getDog called')
   request 
     .get(`${dogApiUrl}`)
     .then(response => dispatch({
       type: 'FETCHED_IMAGE',
       payload: {
-        img: response.body.message
+        img: response.body.message,
+        breed: response.body.message.split('/')[4]
       }
     }))
     .catch(err => alert(err))
 }
 
-export const rateDog = (opinion) => dispatch => {
-  const { breed, type } = opinion
-
-  //request
-    //.get(`${dogApiUrl}`)
-    //.then(response => dispatch(
-      //switch (type) {
-        //case 'like' :
-          //{
-            //type: LIKE_DOG,
-            //payload: {
-              //breed: breed
-            //}
-          //}
-        //case 'dislike':
-          //{
-            //type: DISLIKE_DOG,
-            //payload: {
-              //breed: breed
-            //}
-          //}
-
-      //}
-    //)).catch(err => alert(err))
-  
+export const rateDog = (breed, opinion) => dispatch => {
+  const likeOrDislike = ( (opinion) === 'like' ? LIKE_DOG : DISLIKE_DOG )
+  dispatch({
+    type: likeOrDislike,
+    payload: {
+      breed
+    }
+  })
 }
 
