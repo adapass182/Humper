835cef3ccf188cd96e9502e78101f3dceeb507d2
adapass182 <adampassingham90@gmail.com>
2018-05-14 16:26:08 +0200

    removing reducers conflicts

--- src/reducers/fetchedImage_reducer.js
+++ src/reducers/fetchedImage_reducer.js
@@ -1,22 +1,8 @@
 export default (state = [], { type, payload } = {}) => {
   switch (type) {
-  case `FETCHED_IMAGE`:
+  case 'FETCHED_IMAGE':
     return payload
   default:
     return state
   }
 }
-
-
-/* State shape:
-
-img_001 {
-  id: #001
-  breed: breed
-    reducers
-
-  linkToImg:
-     master
-}
-
-*/
---
+++ src/reducers/likeDislikeDog_reducer.js
@@ -0,0 +1,17 @@
+export default (state = 0, { type, payload }) => {
+  switch (type) {
+  case 'LIKE_DOG':
+  //NOTE: Payload needs to add new key:value to store if no record of this breed exists for this user.
+  //      If breed already exists, needs to increment rating value.
+    return {
+      ...state,
+
+    }
+  case 'DISLIKE_DOG':
+  //NOTE: Payload needs to add new key:value to store if no record of this breed exists for this user.
+  //      If breed already exists, needs to decrement rating value.
+    return state.concat(payload)
+  default:
+    return state
+  }
+}
